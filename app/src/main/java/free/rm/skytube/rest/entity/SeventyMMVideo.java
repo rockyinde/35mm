d8856f8b5a6cb327438061034b719440aaa49c74
Rocky <ri.chkmglr@gmail.com>
2018-12-02 23:07:22 +0530

    REST library integration

---
+++ app/src/main/java/free/rm/skytube/rest/entity/SeventyMMVideo.java
@@ -0,0 +1,14 @@
+package free.rm.skytube.rest.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SeventyMMVideo {
+
+    private String vid;
+    private String title;
+    private String category;
+    private String json;
+}
